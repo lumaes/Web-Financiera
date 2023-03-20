a682eaffa6cb4865e1f00414e8048eb7b000873a
Lumaes <lumaes@live.com.ar>
2023-03-19 23:07:06 -0300

    Web Finanzas

---
+++ Cotizaciones/Controllers/CalculadorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cotizaciones.Controllers
+{
+    public class CalculadorController : Controller
+    {
+        // GET: Calculador
+        public ActionResult Calculador()
+        {
+            return View();
+        }
+    }
+}
---
+++ Cotizaciones/Scripts/Index.js
@@ -0,0 +1,19 @@
+﻿$('.top-line').after('<div class="mobile-menu d-xl-none">');
+	$('.top-menu').clone().appendTo('.mobile-menu');
+	$('.mobile-menu-btn').click(function(){
+		$('.mobile-menu').stop().slideToggle();
+	});
+$(document).on('click', '.panel-heading span.clickable', function (e) {
+    var $this = $(this);
+	if(!$this.hasClass('panel-collapsed')) {
+		$this.parents('.panel').find('.panel-body').slideDown();
+		$this.addClass('panel-collapsed');
+		$this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+		
+	} else {
+		$this.parents('.panel').find('.panel-body').slideUp();
+		$this.removeClass('panel-collapsed');
+		$this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+		
+	}
+})
